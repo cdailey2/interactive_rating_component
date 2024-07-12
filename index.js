2c0ca13fae20e98d7c97f144cdc59e8e232cf6f3
Colin Dailey <167919408+cdailey2@users.noreply.github.com>
2024-07-11 20:02:42 -0400

    Update index.js
    Added interactivity

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const submitButton = document.getElementById("submit-button");
+const ratingContainer = document.getElementById("rating-component");
+const thanksContainer = document.getElementById("thank-you-component");
+const ratings = document.querySelectorAll(".rating-button");
+const response = document.getElementById("your-response");
 
+
+ratings.forEach((rating) => {
+    rating.addEventListener("focus", (event) => {
+        submitButton.addEventListener("click", () => {
+            
+            // Change inner html for "#your-response div"
+            response.textContent = `You selected ${event.target.id} out of 5`
+
+            //Switch rating and thanks containers
+            ratingContainer.classList.toggle("hidden");
+            thanksContainer.classList.toggle("hidden");
+        })
+    })
+})
